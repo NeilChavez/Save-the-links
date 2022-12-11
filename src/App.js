4e14239ee98c1b734f33f274eba5eafa91dc478b
NeilChavez <lolochavez22@icloud.com>
2022-12-11 16:19:26 -0500

    Add route protected component to allow access only to authenticated users

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -16,12 +17,19 @@ function App() {
       <AuthContextProvider>
         <NavBar />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
         {/*           
         <Links />
         <ToastContainer /> */}
