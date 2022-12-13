be6dc7533414d058a65553e6c129ca60ac32abe8
NeilChavez <lolochavez22@icloud.com>
2022-12-13 18:15:24 -0500

    Add responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../hooks/useUser";
 import "./Navbar.css";
@@ -6,18 +7,43 @@ export default function NavBar() {
   // TODO extrae isLogged de useAuth;
   // let isLogged = true;
   const { logout, isLogged } = useUser();
-
+  const [showMenu, SetShowMenu] = useState(false)
+  const handleClick = () => {
+    SetShowMenu(prevState => !prevState);
+  }
   return (
-    <nav className="navbar">
+    <nav className="navbar" onClick={() => showMenu && SetShowMenu(false)}>
       <ul className="navbar-list">
         <li className="navbar-list-item">
-          <Link className="navbar-list-link home-title" to="/">
+          <Link
+            className="navbar-list-link home-title"
+            to="/">
             Home
           </Link>
         </li>
         <li className="navbar-list-item">
-          <div className="login-register-wrapper">
-            {isLogged ? (
+
+          <button class={`hamburger hamburger--collapse 
+            ${showMenu ? "is-active" : ""} btn-hamburger`}
+            type="button"
+            onClick={handleClick}>
+            <span class="hamburger-box">
+              <span class="hamburger-inner hamburger-color"></span>
+            </span>
+          </button>
+          <div className={`login-register-wrapper ${showMenu ? "is-active" : ""}`}>
+
+            <Link
+              className="navbar-list-link link"
+              to="/login">
+              Login
+            </Link>
+            <Link
+              className="navbar-list-link link"
+              to="/register">
+              Register
+            </Link>
+            {/* {isLogged ? (
               <>
                 <Link className="navbar-list-link link" to="/dashboard">
                   My page
@@ -35,7 +61,10 @@ export default function NavBar() {
                   Register
                 </Link>
               </>
-            )}
+            )} */}
+
+
+
           </div>
         </li>
       </ul>
